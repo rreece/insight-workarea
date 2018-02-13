0b9ad10ca9b1fe1abce209a8a0bb01083c64fdf9
Ryan Reece <ryan.reece@cern.ch>
2018-02-13 15:59:14 -0500

    preprocessing for mnist

--- preprocess_cifar.py
+++ preprocess_cifar.py
@@ -27,6 +27,7 @@ AUTHOR
 #------------------------------------------------------------------------------
 
 import argparse, sys, time
+import os
 import pickle
 
 import keras
@@ -60,64 +61,88 @@ def main():
         (x_train, y_train), (x_test, y_test) = cifar10.load_data()
         print(x_train.shape) # (50000, 32, 32, 3)
         print(x_test.shape)  # (10000, 32, 32, 3)
+        print(y_train.shape) # (50000, 1)
         print(y_train) 
         stuff = dict()
         stuff['data'] = x_train
         stuff['labels'] = y_train
-        pickle.dump(stuff, open("cifar-10-train.p", "wb"))
+        pickle.dump(stuff, open("train.p", "wb"))
         del stuff
         stuff = dict()
         stuff['data'] = x_test
         stuff['labels'] = y_test
-        pickle.dump(stuff, open("cifar-10-test.p", "wb"))
+        pickle.dump(stuff, open("test.p", "wb"))
+        cmd = 'tar cvzf cifar-10-train.tar.gz train.p'
+        os.system(cmd)
+        cmd = 'tar cvzf cifar-10-test.tar.gz test.p'
+        os.system(cmd)
 
     elif _type == 'cifar100':
         from keras.datasets import cifar100
         (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
         print(x_train.shape) # (50000, 32, 32, 3)
         print(x_test.shape)  # (10000, 32, 32, 3)
+        print(y_train.shape) # (50000, 1)
         print(y_train) 
         stuff = dict()
         stuff['data'] = x_train
         stuff['labels'] = y_train
-        pickle.dump(stuff, open("cifar-100-train.p", "wb"))
+        pickle.dump(stuff, open("train.p", "wb"))
         del stuff
         stuff = dict()
         stuff['data'] = x_test
         stuff['labels'] = y_test
-        pickle.dump(stuff, open("cifar-100-test.p", "wb"))
+        pickle.dump(stuff, open("test.p", "wb"))
+        cmd = 'tar cvzf cifar-100-train.tar.gz train.p'
+        os.system(cmd)
+        cmd = 'tar cvzf cifar-100-test.tar.gz test.p'
+        os.system(cmd)
 
     elif _type == 'cifar100_coarse':
         from keras.datasets import cifar100
         (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='coarse')
         print(x_train.shape) # (50000, 32, 32, 3)
         print(x_test.shape)  # (10000, 32, 32, 3)
+        print(y_train.shape) # (50000, 1)
         print(y_train) 
         stuff = dict()
         stuff['data'] = x_train
         stuff['labels'] = y_train
-        pickle.dump(stuff, open("cifar-100-coarse-train.p", "wb"))
+        pickle.dump(stuff, open("train.p", "wb"))
         del stuff
         stuff = dict()
         stuff['data'] = x_test
         stuff['labels'] = y_test
-        pickle.dump(stuff, open("cifar-100--coarse-test.p", "wb"))
+        pickle.dump(stuff, open("test.p", "wb"))
+        cmd = 'tar cvzf cifar-100-coarse-train.tar.gz train.p'
+        os.system(cmd)
+        cmd = 'tar cvzf cifar-100-coarse-test.tar.gz test.p'
+        os.system(cmd)
 
     elif _type == 'mnist':
         from keras.datasets import mnist
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        print(x_train.shape) # (50000, 32, 32, 3)
-        print(x_test.shape)  # (10000, 32, 32, 3)
+        print(x_train.shape) # (60000, 28, 28)
+        print(x_test.shape)  # (10000, 28, 28)
+        print(y_train.shape) # (60000,)
+        assert len(y_train.shape) == 1
+        y_train = y_train.reshape(y_train.shape[0], 1)
+        y_test  = y_test.reshape(y_test.shape[0], 1)
+        print(y_train.shape) # (60000, 1)
         print(y_train) 
         stuff = dict()
         stuff['data'] = x_train
         stuff['labels'] = y_train
-        pickle.dump(stuff, open("mnist-train.p", "wb"))
+        pickle.dump(stuff, open("train.p", "wb"))
         del stuff
         stuff = dict()
         stuff['data'] = x_test
         stuff['labels'] = y_test
-        pickle.dump(stuff, open("mnist-test.p", "wb"))
+        pickle.dump(stuff, open("test.p", "wb"))
+        cmd = 'tar cvzf mnist-train.tar.gz train.p'
+        os.system(cmd)
+        cmd = 'tar cvzf mnist-test.tar.gz test.p'
+        os.system(cmd)
 
     elif _type == 'cifar10_local':
         ## official cifar-10
@@ -134,7 +159,7 @@ def main():
         assert len(ops.infiles) == 1
         infile = ops.infiles[0]
         f = open(infile, 'rb')
-        d = pickle.load(f, encoding='bytes')
+        d = pickle.load(f)
         x = d['data']
         print(x.shape) # (50000, 3, 32, 32)
         y = d['labels']
@@ -146,7 +171,7 @@ def main():
         assert len(ops.infiles) == 1
         infile = ops.infiles[0]
         f = open(infile, 'rb')
-        d = pickle.load(f, encoding='bytes')
+        d = pickle.load(f)
         x = d['data']
         print(x.shape)
         y = d['labels']
